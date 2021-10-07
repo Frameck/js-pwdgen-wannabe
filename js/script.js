537fc8906e3ebcb083ec0e4d36c83477a4da4aec
Frameck <fra9879@gmail.com>
2021-10-07 13:06:36 +0200

    Completed exercise

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const firstName = prompt('Inserisci il tuo nome')
+const lastName = prompt('Inserisci il tuo cognome')
+const color = prompt('Qual è il tuo colore preferito')
+
+// const pw = `${firstName}${lastName}${color}21`
+const pw = firstName + lastName + color + 21
+alert(`La tua password è ${pw}`)
