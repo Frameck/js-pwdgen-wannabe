1ebb42e30ce1ecee204568d1776407e275c34138
Francesco Mecchi <77396783+Frameck@users.noreply.github.com>
2021-10-08 10:01:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const firstName = prompt('Inserisci il tuo nome')
 const lastName = prompt('Inserisci il tuo cognome')
-const color = prompt('Qual è il tuo colore preferito')
+const color = prompt('Qual è il tuo colore preferito?')
 
 // const pw = `${firstName}${lastName}${color}21`
 const pw = firstName + lastName + color + 21
